8ac8df125fb6ae07fff6ae265ac0082d5ba2674d
Renato Alexandre Justo <renato.justo@db1.com.br>
2021-01-04 01:16:05 -0300

    Inclusão para classe para enviar e-mail e configuração inicial para testes

--- application/src/main/kotlin/com/arch/application/UserRegisterService.kt
+++ application/src/main/kotlin/com/arch/application/UserRegisterService.kt
@@ -1,17 +1,19 @@
 package com.arch.application
 
+import com.arch.application.domain.User
 import com.arch.application.port.input.UserRegisterCommand
 import com.arch.application.port.input.UserRegisterUseCase
+import com.arch.application.port.output.EmailNotificationPort
 import com.arch.application.port.output.SaveUserPort
-import com.arch.application.domain.User
 import com.arch.shared.annotations.UseCase
 
 @UseCase
-class UserRegisterService(private val saveUserPort: SaveUserPort) : UserRegisterUseCase {
+class UserRegisterService(private val saveUserPort: SaveUserPort,
+                          private val emailNotification: EmailNotificationPort) : UserRegisterUseCase {
 
     override fun registerUser(userRegisterCommand: UserRegisterCommand) {
-        println("Registrando usuario: $userRegisterCommand")
         val user = User(name = userRegisterCommand.name)
         saveUserPort.registerUser(user)
+        emailNotification.send(user)
     }
 }
---
+++ application/src/test/kotlin/com/arch/application/UserRegisterServiceTest.kt
@@ -0,0 +1,5 @@
+package com.arch.application
+
+class UserRegisterServiceTest {
+
+}
