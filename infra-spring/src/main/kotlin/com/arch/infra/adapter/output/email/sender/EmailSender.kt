2996b653fb8606bceebc417ead2470cce40adb82
Renato Alexandre Justo <renato.justo@db1.com.br>
2021-01-06 20:39:21 -0300

    Ajuste no número de retentativa para erros

--- infra-spring/src/main/kotlin/com/arch/infra/adapter/output/email/sender/EmailSender.kt
+++ infra-spring/src/main/kotlin/com/arch/infra/adapter/output/email/sender/EmailSender.kt
@@ -1,8 +1,10 @@
 package com.arch.infra.adapter.output.email.sender
 
 import mu.KotlinLogging
+import org.springframework.amqp.ImmediateAcknowledgeAmqpException
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
+import org.springframework.messaging.handler.annotation.Header
 
 private val logger = KotlinLogging.logger {}
 
@@ -10,9 +12,17 @@ private val logger = KotlinLogging.logger {}
 class EmailSender {
 
     @StreamListener(EmailSink.INPUT)
-    fun send(log: String) {
+    fun send(log: String, @Header(name = "x-death", required = false) death: Map<String, Object>?) {
+        if (RETRY_TIMES.equals(death?.get("count"))) {
+            throw ImmediateAcknowledgeAmqpException("Desistindo da fila")
+        }
         logger.info { "Enviando email: [1]: $log" }
         Thread.sleep(5000)
+        throw RuntimeException("Erro de Rede")
         logger.info { "Email Enviado!" }
+    }
+
+    companion object {
+        const val RETRY_TIMES = 3L;
     }
 }
